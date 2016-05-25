6522fd1d7c46e0db19371db648a8a991a3812cd5
rutamelap <rutamelap@gmail.com>
2016-05-26 00:22:01 +0300

    Lokomotyvo klases pridejimas

---
+++ lokomotyvas.py
@@ -0,0 +1,10 @@
+class Lokomotyvas():
+
+    def __init__(self, l_mase, max_t_mase, l_id):
+        """ Sukuria objekta - lokomotyva. Cia:
+        l_mase - lokomotyvo mase,
+        max_t_mase - maksimali tempiamoji mase,
+        l_id - lokomotyvo unikalus numeris"""
+        self.l_mase = l_mase
+        self.max_t_mase = max_t_mase
+        self.l_id = l_id
