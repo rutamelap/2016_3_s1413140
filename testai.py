d2e76508e43e5fb47381d0be806c45897f60af94
rutamelap <rutamelap@gmail.com>
2016-05-26 01:45:55 +0300

    Testo pradzia. Du nauji patikrinimai

--- testai.py
+++ testai.py
@@ -1,5 +1,18 @@
 import unittest
 import surinktuvas
-import lokomotyvas
 import traukinys
-import vagonas
+#  https://docs.python.org/3.5/library/unittest.html
+#  https://jeffknupp.com/blog/2013/12/09/improve-your-python-understanding-unit-testing/
+
+class TestPridetasLokomotyvas(unittest.TestCase):
+    def test_pridetasLokomotyvas(self):
+        self.assertIsNone(traukinys.Traukinys.pridetasLokomotyvas(
+            traukinys.Traukinys([100, 100, 9876])))
+
+class TestUzimtasLokomotyvas(unittest.TestCase):
+    def test_uzimtasLokomotyvas(self):
+        self.assertIsNone(traukinys.Traukinys.uzimtasLokomotyvas(
+            traukinys.Traukinys(100, 200, 8765)))
+
+if __name__ == '__main__':
+    unittest.main()
